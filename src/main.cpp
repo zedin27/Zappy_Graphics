19da23fe7dff9a8955b059751a4973b0ee75f67c
theo-walton <theodore_walton@hotmail.com>
2018-06-09 14:51:11 -0700

    model updated with new obj renderer

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ int	main(void)
 	Window window(1600, 900, "zap");
 	FreeCamera cam(window);
 	Light light(glm::vec3(500, 1000, 0), glm::vec3(1, 1, 1), 1000000);
-//	Map map(fd, glm::vec2(25, 25));
+	Map map(fd, glm::vec2(25, 25));
 	Time clock;
 	FPSDisplay fps;
 	SkyBox sky("assets/textures/skybox/right.png",
@@ -28,7 +28,7 @@ int	main(void)
 		   "assets/textures/skybox/front.png",
 		   "assets/textures/skybox/back.png");
 
-	Obj obj("assets/meshes/fullface.obj", "assets/textures/white.png");
+//	Obj obj("assets/meshes/fullface.obj", "assets/textures/white.png");
 
 	glClearColor(0.2, 0.25, 0.3, 1);
 
@@ -43,9 +43,9 @@ int	main(void)
 		cam.Update();
 
 		sky.Render(cam.Perspective());
-		obj.Render(cam.Perspective(), glm::mat4(1), glm::vec3(0, 0, 0), 0.005, false);
+//		obj.Render(cam.Perspective(), glm::mat4(1), glm::vec3(0, 0, 0), 0.005, false);
 		
-//		map.Render(cam.Perspective(), clock.Delta());
+		map.Render(cam.Perspective(), clock.Delta());
 
 		fps.Render();
 		window.Render();
