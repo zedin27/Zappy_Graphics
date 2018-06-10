6d05c99c9fe9d280a4ddc43dfbffedcec1b0ca1c
theo-walton <theodore_walton@hotmail.com>
2018-06-09 17:47:47 -0700

    added mesh caching

--- src/ObjFileArrayExtractor.cpp
+++ src/ObjFileArrayExtractor.cpp
@@ -1,5 +1,7 @@
 #include "ObjFileArrayExtractor.hpp"
 
+std::map<std::string, ObjFileArrayExtractor::ObjInfo> ObjFileArrayExtractor::_cache;
+
 void	ObjFileArrayExtractor::Parse_v(std::stringstream &ss)
 {
 	std::string temp;
@@ -74,7 +76,16 @@ void	ObjFileArrayExtractor::Parse_f(std::stringstream &ss)
 }
 
 ObjFileArrayExtractor::ObjFileArrayExtractor(std::string filename)
-{	
+{
+	if (_cache.count(filename) != 0)
+	{
+		ObjInfo info = _cache[filename];
+		_verticesOut = info.vertices;
+		_normalsOut = info.normals;
+		_uvMapOut = info.uv;
+		return;
+	}
+	
 	std::ifstream file(filename);
 
 	if (!file.good())
@@ -102,33 +113,38 @@ ObjFileArrayExtractor::ObjFileArrayExtractor(std::string filename)
 			Parse_f(ss);
 		temp = "";
 	}
+	_verticesOut = new std::vector<float>;
+	_normalsOut = new std::vector<float>;
+	_uvMapOut = new std::vector<float>;
 	for (auto &p : _faces)
 	{
 		
-		_verticesOut.push_back(_vertices[p.vertex * 3 - 3]);
-		_verticesOut.push_back(_vertices[p.vertex * 3 - 2]);
-		_verticesOut.push_back(_vertices[p.vertex * 3 - 1]);
+		_verticesOut->push_back(_vertices[p.vertex * 3 - 3]);
+		_verticesOut->push_back(_vertices[p.vertex * 3 - 2]);
+		_verticesOut->push_back(_vertices[p.vertex * 3 - 1]);
 		
-		_normalsOut.push_back(_normals[p.normal * 3 - 3]);
-		_normalsOut.push_back(_normals[p.normal * 3 - 2]);
-		_normalsOut.push_back(_normals[p.normal * 3 - 1]);
+		_normalsOut->push_back(_normals[p.normal * 3 - 3]);
+		_normalsOut->push_back(_normals[p.normal * 3 - 2]);
+		_normalsOut->push_back(_normals[p.normal * 3 - 1]);
 
-		_uvMapOut.push_back(_uvMap[p.texture * 2 - 2]);
-		_uvMapOut.push_back(_uvMap[p.texture * 2 - 1]);
+		_uvMapOut->push_back(_uvMap[p.texture * 2 - 2]);
+		_uvMapOut->push_back(_uvMap[p.texture * 2 - 1]);
 	}
+	ObjInfo info = {_verticesOut, _normalsOut, _uvMapOut};
+	_cache[filename] = info;
 }
 
 std::vector<float>	&ObjFileArrayExtractor::GetVertices(void)
 {
-	return _verticesOut;
+	return *_verticesOut;
 }
 
 std::vector<float>	&ObjFileArrayExtractor::GetNormals(void)
 {
-	return _normalsOut;
+	return *_normalsOut;
 }
 
 std::vector<float>	&ObjFileArrayExtractor::GetUVMap(void)
 {
-	return _uvMapOut;
+	return *_uvMapOut;
 }
