bc72dc6b312bb618b840a075bb86d23a41f8c047
theo-walton <theodore_walton@hotmail.com>
2018-06-11 18:00:02 -0700

    doesnt require server

--- src/main.cpp
+++ src/main.cpp
@@ -15,39 +15,13 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-int client_socket(char *ip, uint16_t port)
-{
-	struct sockaddr_in	client_addr;
-	int					        sockfd;
-	
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 6)) == -1)
-		return (-1);
-
-	std::cout << sockfd << std::endl;
-	client_addr.sin_family = AF_INET;
-	client_addr.sin_port = htons(port);
-	if ((client_addr.sin_addr.s_addr = inet_addr(ip)) == (in_addr_t)-1)
-    return (-1);
-	if (connect(sockfd, (struct sockaddr*)&client_addr, sizeof(struct sockaddr_in)) == -1)
-	{
-		printf("No bueno %s\n", strerror(errno));		
-		return (-1);
-	}
-	std::cout << sockfd << std::endl;
-	return (sockfd);
-}
-
 int	main(int argc, char *argv[])
 {
-	int	clientfd;
-		
-	if (argc <= 2
-	    || ((clientfd = client_socket(argv[1], atoi(argv[2]))) == -1))
-		return (EXIT_FAILURE);
-
+	int	clientfd = open("testcommands", O_RDONLY);
+	
 	Window window(1600, 900, "zap");
 	FreeCamera cam(window);
-	Light light(glm::vec3(0, 100, 0), glm::vec3(1, 1, 0.2), 100);
+	Light light(glm::vec3(5, 10, 6), glm::vec3(1, 1, 1), 10);
 	Map map(clientfd);
 	Time clock;
 	FPSDisplay fps;
@@ -58,10 +32,6 @@ int	main(int argc, char *argv[])
 		   "assets/textures/skybox/front.png",
 		   "assets/textures/skybox/back.png");
 
-	char buf[10];
-	int lol = read(clientfd, buf, 10);
-	write(clientfd, "GRAPHIC\n", 8);
-	
 	glClearColor(0.2, 0.25, 0.3, 1);
 	while (!window.ShouldClose())
 	{
